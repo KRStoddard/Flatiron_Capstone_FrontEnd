8ba74efc9ccbd5715888712407323cfe4e5ef878
KRStoddard <kimberlynrstoddard@gmail.com>
2020-11-09 12:46:16 -0600

    a little styling

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -4,11 +4,12 @@ import {Link} from 'react-router-dom'
 //renders each individual playlist
 const Playlist = (props) => {
     return(
-        <Link to={`/playlists/${props.playlist.id}`}>
-            <li className="list-group-item">
-                {props.playlist.name}
+        
+            <li className="list-group-item text-decoration-none">
+                <Link to={`/playlists/${props.playlist.id}`}>
+                    {props.playlist.name}
+                </Link>
             </li>
-        </Link>
     )
 }
 
